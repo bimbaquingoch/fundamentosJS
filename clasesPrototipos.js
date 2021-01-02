9e82e6ba2b273919440ecf1ee79aed01b872c5e4
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-01-02 13:09:35 -0500

    tiempos en JS y asincronismo

--- clasesPrototipos.js
+++ clasesPrototipos.js
@@ -1,46 +1,54 @@
-function heredade(prototipoHijo, prototipoPadre) {
-  var fn = function () {};
-  fn.prototype = prototipoPadre.prototype;
-  prototipoHijo.prototype = new fn();
-  //asignar la funcion constructora
-  prototipoHijo.prototype.constructor = prototipoHijo;
+class Persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.altura = altura;
+  }
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(
+      `hola me llamo ${this.nombre} ${this.apellido} y mido ${this.altura}`
+    );
+
+    if (fn) {
+      fn(nombre, apellido);
+    }
+  }
+
+  soyAlto() {
+    var altura =
+      this.altura > ALTURA
+        ? `yo ${this.nombre} soy alto`
+        : `yo ${this.nombre} no soy alto`;
+    return console.log(altura);
+  }
 }
 
-// se define el constructor
-// esto es un prototipo/clase
-function Persona(nombre, apellido, altura) {
-  this.nombre = nombre;
-  this.apellido = apellido;
-  this.altura = altura;
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, altura) {
+    super(nombre, apellido, altura);
+  }
+
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    // estas 2 líneas son equivalentes a la line de arriba
+    // var nombre = this.nombre;
+    // var apellido = this.apellido;
+    console.log(`hola me llamo ${this.nombre} y soy desarrollador/a `);
+    if (fn) {
+      fn(nombre, apellido, true);
+    }
+  }
 }
 
-Persona.prototype.saludar = function () {
-  console.log(
-    `hola me llamo ${this.nombre} ${this.apellido} y mido ${this.altura}`
-  );
-};
-
-function Desarrollador(nombre, apellido) {
-  this.nombre = nombre;
-  this.apellido = apellido;
+function responderSaludo(nombre, apellido, esDev) {
+  console.log(`Buen día ${nombre} ${apellido}`);
+  if (esDev) {
+    console.log(`Ah mira no sabia que eres desarrollador `);
+  }
 }
-
-// aquí se aplica le herencia de la función qe se creó al inicio
-heredade(Desarrollador, Persona);
-
-Desarrollador.prototype.saludar = function () {
-  console.log(`hola me llamo ${this.nombre} y soy desarrollador `);
-};
 
 const ALTURA = 1.8;
-
-Persona.prototype.soyAlto = function () {
-  var altura =
-    this.altura > ALTURA
-      ? `yo ${this.nombre} soy alto`
-      : `yo ${this.nombre} no soy alto`;
-  return console.log(altura);
-};
 
 // new hace que se cree un nuevo objeto
 var eren = new Persona("Eren", "Jagger", 1.7);
@@ -49,5 +57,9 @@ var levi = new Persona("Levi", "Ackerman", 1.6);
 
 var erwin = new Persona("Erwin", "Smith", 1.88);
 
-var bryan = new Desarrollador("Bryan", "Imbaquingo");
-bryan.saludar();
+var bryan = new Desarrollador("Bryan", "Imbaquingo", 1.7);
+
+eren.saludar();
+levi.saludar(responderSaludo);
+erwin.saludar();
+bryan.saludar(responderSaludo);
