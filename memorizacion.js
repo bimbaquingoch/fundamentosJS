84ef41b8df506bd1b6cdeece6b1521c2c34367b3
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-01-03 13:00:28 -0500

    memorizacion

---
+++ memorizacion.js
@@ -0,0 +1,26 @@
+/*
+memorizacion es otro caso de recursividad o se resuelve
+con recursividad
+!6 = 6*5*4*3*2*1=720
+!12= 12*11*10*9*8*7*!6 = 
+*/
+
+function factorial(n) {
+  if (!this.cache) {
+    this.cache = {};
+  }
+  debugger;
+  if (this.cache[n]) {
+    return this.cache[n];
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  this.cache[n] = n * factorial(n - 1);
+  debugger;
+  return this.cache[n];
+}
+
+// console.log(factorial(6));
